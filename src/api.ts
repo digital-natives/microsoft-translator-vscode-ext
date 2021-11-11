a8d19c1ee93d51f91aa5228ce99a4d2153320acb
Takeshi <iwatakeshi@gmail.com>
2021-11-11 09:44:38 -0700

    Refactor headers

--- src/api.ts
+++ src/api.ts
@@ -18,18 +18,16 @@ type TranslationResponse = AxiosResponse<
 
 export function translate(text: string, options: TranslateOptions) {
   const clientTraceId = v4()
-  const region = options.subscriptionRegion
-    ? { 'Ocp-Apim-Subscription-region': options.subscriptionRegion }
-    : {}
+
   return axios.post<any, TranslationResponse>(
     `https://api.cognitive.microsofttranslator.com/translate?api-version=3.0&to=${options.language}`,
     [{ Text: text }],
     {
       headers: {
-        'Ocp-Apim-Subscription-Key': options.subscriptionKey,
+        'Ocp-Apim-Subscription-Key': options.subscriptionKey ?? '',
         'Content-type': 'application/json',
         'X-ClientTraceId': clientTraceId,
-        ...region,
+        'Ocp-Apim-Subscription-region': options.subscriptionRegion ?? ''
       },
     }
   )
